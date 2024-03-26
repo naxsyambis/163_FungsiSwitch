d468a2fed5af6aaaa622b01ba46ca51595076dc6
naxsyambis <naxsyambis@gmail.com>
2024-03-26 10:11:08 +0000

    membuat prosedur dan fungsi luas

--- Fungsi Switch/Fungsi Switch.cpp
+++ Fungsi Switch/Fungsi Switch.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int luas;
+
+void prosedurluas(int p, int l)
+{
+    luas = p * l;
+}
+
+int fungsiluas(int p, int l)
+{
+    return p * l;
+}
 
 int main()
 {
