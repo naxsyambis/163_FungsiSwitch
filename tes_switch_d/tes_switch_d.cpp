bf30ba0350cd7f34fd612313919ec2626bc47600
naxsyambis <naxsyambis@gmail.com>
2024-03-26 11:13:10 +0000

    membuat fungsi penjumlahan, pengurangan, perkalian, dan pembagian

---
+++ tes_switch_d/tes_switch_d.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int bilangan1, bilangan2;
+
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+
+int pengurangan(int a, int b)
+{
+    return a - b;
+}
+
+int perkalian(int a, int b)
+{
+    return a * b;
+}
+
+float pembagian(int a, int b)
+{
+    return a / b;
+}
+
+int main()
+{
+
+}
