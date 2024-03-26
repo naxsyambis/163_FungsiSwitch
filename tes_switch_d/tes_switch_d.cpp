19cfd98b157d92bb69e15bdb4e51693d39f5fe59
naxsyambis <naxsyambis@gmail.com>
2024-03-26 11:24:57 +0000

    membuat looping do while

--- tes_switch_d/tes_switch_d.cpp
+++ tes_switch_d/tes_switch_d.cpp
@@ -34,5 +34,47 @@ void inputData()
 int main()
 {
     int pilihan;
-   
+    do {
+        system("CLS");
+        cout << "Menu Kalkulator sederhana" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Masukkan pilihan : ";
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
+        case 1:
+            inputData();
+            cout << "\nHasil penjumlahan: " << penjumlahan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 2:
+            inputData();
+            cout << "\nHasil pengurangan: " << pengurangan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 3:
+            inputData();
+            cout << "\nHasil perkalian: " << perkalian(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 4:
+            inputData();
+            cout << "\nHasil pembagian: " << pembagian(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 5:
+            break;
+        default:
+            cout << "Pilihan tidak valid" << endl;
+            system("pause");
+            break;
+
+        }
+    } while (pilihan != 5); //!= adalah tidak sama dengan
+
 }
